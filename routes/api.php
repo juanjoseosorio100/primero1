d24d4bea3f326ccf618f1d8180e5266bada31d92
juanjoseosorio100 <juan1101oso@gmail.com>
2022-04-04 19:40:43 -0500

    tewrcer cambio

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+/*holasddcfw*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/*holasddcfw*/
