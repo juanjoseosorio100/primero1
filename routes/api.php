9b3597916ac0c40077dee3d9ce13452699a02b7c
juanjoseosorio100 <juan1101oso@gmail.com>
2022-04-04 19:42:49 -0500

    te cambio

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 /*holasddcfw*/
 
+/*233*/
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
